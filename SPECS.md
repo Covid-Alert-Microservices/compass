21b2cbf245b63a908c0d61db7dc6edc44d2c3d5d
duvalax <axel.duval74@gmail.com>
2021-11-09 10:40:27 +0100

    Add Sequence diagrams

---
+++ SPECS.md
@@ -0,0 +1,15 @@
+# Sequence diagrams
+
+### Handling geolocation requests
+
+![Seq1](/assets/Seq1.png)
+
+### Using Neo4j for optimization 
+
+
+![Seq2](/assets/Seq2.png)
+
+### Positive case
+
+![Seq3](/assets/Seq3.png)
+
--- src/test/java/com/github/covidalert/microservicetemplate/GeolocationApplicationTests.java
+++ src/test/java/com/github/covidalert/microservicetemplate/GeolocationApplicationTests.java
@@ -1,7 +1,10 @@
 package com.github.covidalert.microservicetemplate;
 
+import com.github.covidalert.microservicetemplate.models.Geolocation;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -11,9 +14,16 @@ import org.springframework.test.annotation.DirtiesContext;
 class GeolocationApplicationTests
 {
 
+    @Autowired
+    private KafkaTemplate<String, String> stringKafkaTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, Geolocation> geolocationKafkaTemplate;
+
     @Test
-    void contextLoads()
-    {
+    public void kafkaTest() throws Exception {
+        geolocationKafkaTemplate.send("geolocation_created", new Geolocation("12345",10.000,10.000,1L));
+        stringKafkaTemplate.send("user_positive", "12345");
     }
 
 }
