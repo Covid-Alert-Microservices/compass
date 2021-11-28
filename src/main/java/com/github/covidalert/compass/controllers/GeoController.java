ccea6e790d1621e79dba7e88f185e216edfee67d
BrokenSwing <fhugouvieux@gmail.com>
2021-11-28 22:16:52 +0100

    Add logging

--- src/main/java/com/github/covidalert/compass/controllers/GeoController.java
+++ src/main/java/com/github/covidalert/compass/controllers/GeoController.java
@@ -3,9 +3,14 @@ package com.github.covidalert.compass.controllers;
 import com.github.covidalert.compass.dtos.NewGeolocationDto;
 import com.github.covidalert.compass.models.Geolocation;
 import com.github.covidalert.compass.repositories.GeolocationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -16,6 +21,8 @@ import java.security.Principal;
 public class GeoController
 {
 
+    private final Logger logger = LoggerFactory.getLogger(GeoController.class);
+
     @Autowired
     private GeolocationRepository geolocationRepository;
 
@@ -23,12 +30,16 @@ public class GeoController
     private KafkaTemplate<String, Geolocation> geolocationKafkaTemplate;
 
     @PostMapping
-    public Geolocation addUserGeolocation(Principal principal, @Valid @RequestBody NewGeolocationDto geolocation )
+    public Geolocation addUserGeolocation(Principal principal, @Valid @RequestBody NewGeolocationDto geolocation)
     {
         String userId = "123-456";
-        if(principal != null && principal.getName().length() > 0) userId = principal.getName();
+        if (principal != null && principal.getName().length() > 0)
+        {
+            userId = principal.getName();
+            this.logger.warn(String.format("New geolocation from unknown user. Falling back to id %s.", userId));
+        }
         Geolocation document = new Geolocation(userId, geolocation.getLatitude(), geolocation.getLongitude(), geolocation.getTimestamp());
-        Geolocation saved =  geolocationRepository.save(document);
+        Geolocation saved = geolocationRepository.save(document);
         geolocationKafkaTemplate.send("geolocation_created", document);
         return saved;
     }
--- src/main/java/com/github/covidalert/compass/listeners/GeoListener.java
+++ src/main/java/com/github/covidalert/compass/listeners/GeoListener.java
@@ -5,6 +5,8 @@ import com.github.covidalert.compass.models.Geolocation;
 import com.github.covidalert.compass.repositories.GeolocationRepository;
 import com.github.covidalert.compass.repositories.GraphRepository;
 import com.github.covidalert.covidtests.dtos.UserPositiveDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -18,6 +20,8 @@ public class GeoListener {
     public static final int VIRUS_DISTANCE_METERS = 8;
     public static final int INCUBATION_TIME_MILLIS = 9 * 24 * 60 * 60 * 1000; // 9 days
 
+    private final Logger logger = LoggerFactory.getLogger(GeoListener.class);
+
     @Autowired
     private GeolocationRepository geolocationRepository;
 
@@ -30,20 +34,47 @@ public class GeoListener {
     @KafkaListener(topics = "user_positive", containerFactory = "userKafkaListener")
     public void userPositive(UserPositiveDTO userPositiveDto) {
         List<String> potentialIds = graphRepository.findPotentials(userPositiveDto.getUserId(), userPositiveDto.getTimestamp() - INCUBATION_TIME_MILLIS);
+        this.logger.debug(String.format(
+                "Received new positive user for %s from Kafka topic `user_positive`. %d users potentially infected.",
+                userPositiveDto.getUserId(),
+                potentialIds.size()
+        ));
         for (String id : potentialIds) {
             this.stringKafkaTemplate.send("send_alert", id);
+            this.logger.trace(String.format("Sent message on `send_alert` kafka topic for user %s.", id));
         }
     }
 
     @KafkaListener(topics = "geolocation_created", containerFactory = "geolocationKafkaListener")
     public void populateGraph(Geolocation geolocation) {
         List<Geolocation> potentialInfectedGeolocations = geolocationRepository.potentialInfectedGeolocations(geolocation.getUserId(), geolocation.getLatitude(), geolocation.getLongitude(), geolocation.getTimestamp() - VIRUS_TIME_MILLIS);
+        int insertedCount = 0;
         for (Geolocation geo : potentialInfectedGeolocations) {
             double distance = Helper.distance(geolocation.getLatitude(), geolocation.getLongitude(), geo.getLatitude(), geo.getLongitude());
+            this.logger.trace(String.format(
+                    "Geolocation %f/%f (user %s) is %f meters away from %f/%f (user %s). Will insert if %f <= %d",
+                    geo.getLatitude(),
+                    geo.getLongitude(),
+                    geo.getUserId(),
+                    distance,
+                    geolocation.getLatitude(),
+                    geolocation.getLongitude(),
+                    geolocation.getUserId(),
+                    distance,
+                    VIRUS_DISTANCE_METERS
+            ));
             if (distance <= VIRUS_DISTANCE_METERS) {
                 graphRepository.insert(geolocation.getUserId(), geo.getUserId(), geolocation.getTimestamp());
+                insertedCount += 1;
             }
         }
+        this.logger.debug(String.format(
+                "Found %d potential infected geolocations around user %s. Inserted %d/%d in graph.",
+                potentialInfectedGeolocations.size(),
+                geolocation.getUserId(),
+                insertedCount,
+                potentialInfectedGeolocations.size()
+        ));
     }
 
 }
